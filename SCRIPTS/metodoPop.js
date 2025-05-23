7947ce599d791562a724f5a403c2acee83224f12
Thales971 <thales.t.silva6@aluno.senai.br>
2025-05-23 09:44:23 -0300

    feat: Usa  metodo pop e adiciona arquivo README.md

---
+++ SCRIPTS/metodoPop.js
@@ -0,0 +1,16 @@
+let playlist = ["Funk","hip-hop","pop"];
+
+console.log
+("Antes do pop");
+console.table(playlist);
+
+// Removendo ultimo estilo
+let estiloRemovido = playlist.pop();
+
+console.log("Depois do pop");
+console.log(playlist);
+
+console.log
+("Estilo Removido:", estiloRemovido)
+
+
